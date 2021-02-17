ade2ddff67e47df9911167cbccc196094859db8a
sgranlund <granlundsebastian7@gmail.com>
2021-02-17 13:30:18 +0100

    subscriber

--- hk_ros_2021/scripts/subscriberAT9.py
+++ hk_ros_2021/scripts/subscriberAT9.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python2
-
-# Example how to generate the output file
-
-import yaml
-import rospkg
-import subscriberAT9
 #!/usr/bin/env python  
 
 import rospy
@@ -48,25 +41,3 @@ if __name__ == '__main__':
         print tagTable
         rate.sleep()
            
-# 1 create an empty list to store the detections
-
-detections = []
-
-# 2 append detections during the run
-# remember to add logic to avoid duplicates
-
-# first dummy detection (apriltag)
-detections.append({"obj_type": "A", "XY_pos": [0.756,3.332]})
-
-# second dummy detection (geometric shape)
-detections.append({"obj_type": "B", "XY_pos": [3.396,0.123]})
-
-# third dummy detection (animal)
-detections.append({"obj_type": "C", "XY_pos": [6.001,2.987]})   
-    
-# 3 save the file
-filename = "latest_output_file.yaml"
-filepath = rospkg.RosPack().get_path('hk_ros_2021') + '/exported_detection_logs/' 
-
-with open(filepath + filename, 'w') as outfile:
-    yaml.dump_all(detections, outfile,explicit_start=True)
